b2a63ccd4f312f4cb54e7f90493d6fb110326ea1
Nick Schoeb <nickschoeb@users.noreply.github.com>
2024-04-02 16:33:24 -0400

    Create baseline_cpp_windows_vsfull.cpp
    added baseline code C++ windows vs full

---
+++ baselines/cpp/baseline_cpp_windows_vsfull.cpp
@@ -0,0 +1,30 @@
+// Baseline code for C/C++ courses:
+// CIS 115
+// Windows
+// Visual Studio (Full version not VS Code)
+
+/*
+Author:			Your Name
+Description:	Your Description
+*/
+
+// Allowed Warnings
+#pragma warning (disable:6031) // getch
+#pragma warning (disable:4996) // scanf
+
+// C
+#include <stdio.h>
+#include <conio.h>
+
+// C++
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	// add your code here
+
+	// Exit to OS
+	_getch();
+	return 0;
+}
